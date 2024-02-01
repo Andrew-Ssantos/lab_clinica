4987297cef8d6900ef5446f13822d57bc318367a
Andrew-Ssantos <andrew-s-silva@outlook.com>
2024-01-31 21:42:24 -0300

    Implementado flutter_getit e async loader, finalizada o básico do core

--- lab_clinica_core/lib/src/helpers/messages.dart
+++ lab_clinica_core/lib/src/helpers/messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -22,5 +23,58 @@ final class Messages {
       Overlay.of(context),
       CustomSnackBar.success(message: successMessage),
     );
+  }
+}
+
+mixin MessageStateMixin {
+  final Signal<String?> _errorMessage = signal(null);
+  String? get errorMessage => _errorMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMessage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMessage => _successMessage();
+
+  void clearError() => _errorMessage.value = null;
+  void clearInfo() => _infoMessage.value = null;
+  void clearSuccess() => _successMessage.value = null;
+
+  void showError(String errorMessage) {
+    untracked(() => clearError());
+    _errorMessage.value = errorMessage;
+  }
+
+  void showInfo(String infoMessage) {
+    untracked(() => clearInfo());
+    _infoMessage.value = infoMessage;
+  }
+
+  void showSuccess(String successMessage) {
+    untracked(() => clearSuccess());
+    _successMessage.value = successMessage;
+  }
+
+  void clearMessages() {
+    untracked(() {
+      clearError();
+      clearInfo();
+      clearSuccess();
+    });
+  }
+}
+
+mixin MessageViewMixin<T extends StatefulWidget> on State<T> {
+  void messageListener(MessageStateMixin state) {
+    effect(() {
+      switch (state) {
+        case MessageStateMixin(:final errorMessage?):
+          Messages.showError(errorMessage, context);
+        case MessageStateMixin(:final infoMessage?):
+          Messages.showInfo(infoMessage, context);
+        case MessageStateMixin(:final successMessage?):
+          Messages.showSuccess(successMessage, context);
+      }
+    });
   }
 }
